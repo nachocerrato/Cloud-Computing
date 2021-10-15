4578c954b02e05f0fb451777de85946e7e2ce8fe
nachocerrato <nacho.cr.94@gmail.com>
2021-10-15 19:03:31 +0200

    commit

--- ComparaNumeros.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Introduce el primer número: "))
-b = int(input("Introduce el segundo número: "))
-if (a < b):
-    print(b)
-else:
-    print(a)
--- TablaMulti.py
+++
@@ -1,6 +0,0 @@
-numero = int(input("Dime un número hostias: "))
-
-i = 1
-
-for i in range(1,11):
-    print(numero, " * ", i, " = ", numero * i)
