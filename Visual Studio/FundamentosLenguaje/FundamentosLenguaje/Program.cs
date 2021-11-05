a091927dff9f17af2de459b2e0fe85c0fbdabecb
nachocerrato <nacho.cr.94@gmail.com>
2021-11-05 15:11:46 +0100

    commit

--- Visual Studio/FundamentosLenguaje/FundamentosLenguaje/Program.cs
+++ Visual Studio/FundamentosLenguaje/FundamentosLenguaje/Program.cs
@@ -6,27 +6,73 @@ namespace FundamentosLenguaje
     {
         static void Main(string[] args)
         {
-            NumeroPositivoNegativo();
+            //NumeroPositivoNegativo();
+            //ConjeturaCollatz();
+            SumarNumeros();
+        }
+
+
+      
+        static void ConjeturaCollatz()
+        {
+            Console.WriteLine("Escriba un número: ");
+            String dato = Console.ReadLine();
+            int numero = int.Parse(dato);
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    numero = numero / 2;
+                }
+                else
+                {
+                    numero = numero * 3 + 1;
+                }
+                Console.WriteLine(numero);
+            }
         }
 
         static void NumeroPositivoNegativo()
         {
-            Console.WriteLine("Introduzca un número: ");
-            //Recupero valor de variable string
+            Console.WriteLine("Introduzca un número");
+            //RECUPERO EL VALOR EN UNA VARIABLE STRING
             String dato = Console.ReadLine();
+            //DECLARAMOS UN int PARA CONVERTIR EL DATO
             int numero = int.Parse(dato);
-            //Evaluamos con condición
+            //EVALUAMOS CON UNA CONDICION POSITIVO, NEGATIVO O CERO
             if (numero > 0)
             {
-                Console.WriteLine("Positivo")
-            } else if (numero == 0)
+                Console.WriteLine("POSITIVO");
+            }
+            else if (numero == 0)
             {
-                Console.WriteLine("Cero");
-            } else
+                Console.WriteLine("CERO");
+            }
+            else
             {
-                Console.WriteLine("Negativo");
+                Console.WriteLine("NEGATIVO");
+            }
+        }
+
+        static void SumarNumeros()
+        {
+            Console.WriteLine("Introduzca un número: ");
+            string dato = Console.ReadLine();
+            int numero = int.Parse(dato);
+            int total = numero;
+            int total_ant = 0;
+
+            while (numero != 0)
+            {
+                Console.WriteLine("Total: " + total_ant + " + " + numero + " = " + total);
+                Console.WriteLine("Introduzca un número: ");
+                dato = Console.ReadLine();
+                numero = int.Parse(dato);
+                total_ant = total;
+                total = total + numero;
             }
 
+            Console.WriteLine("Fin del programa");
         }
     }
 }
